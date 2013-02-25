525b78167f3ee6f79bbeac2df9d8c6fdf0dc1e90
Jason Kotchoff <cornflakesuperstar@hotmail.com>
2013-02-25 21:15:37 +1100

    added upload error image

--- app/controllers/admin/tinymce_assets_controller.rb
+++ app/controllers/admin/tinymce_assets_controller.rb
@@ -1,14 +1,20 @@
 class Admin::TinymceAssetsController < Admin::BaseController
   def create
-    @page = Page.find(params[:hint])
+    image_url = nil
+    if params[:hint].blank?
+      image_url = '/assets/not_saved_upload_error.png'
+    else
+      @page = Page.find(params[:hint])
+      image_record = @page.content_images.new
+      image_record.image = params[:file]
+      image_record.save
+      image_url = image_record.image_url(:standard)
+    end
     
-    image_record = @page.content_images.new
-    image_record.image = params[:file]
-    image_record.save
     
     render json: {
       image: {
-        url: image_record.image_url(:standard)
+        url: image_url
       }
     }, content_type: "text/html"
   end
