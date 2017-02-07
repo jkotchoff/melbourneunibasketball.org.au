61aa70d82ec8ca3648951c855ba1ab82a6aa17f6
Jason Kotchoff <jason@geoplus.com.au>
2017-02-07 11:02:01 +1100

    excluding ineligible numbers from uniform csv

--- lib/member_database.rb
+++ lib/member_database.rb
@@ -3,7 +3,10 @@ class MemberDatabase
 
   def self.uniforms_csv
     filepath = "#{Rails.root}/tmp/melbourne_uni_basketball_members_#{Time.now.to_s(:file_timestamp)}.csv"
-    months_reported = 24
+    months_reported = 12
+
+    # https://au.answers.yahoo.com/question/index?qid=20090521061008AAQ5lyM
+    eligible_numbers = [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45, 50, 52, 52, 53, 54, 55]
     CSV.open(filepath, "wb") do |csv|
       current_mens_numbers = {}
       current_womens_numbers = {}
@@ -27,18 +30,18 @@ class MemberDatabase
         end
       end
 
-      csv << ["Mens numbers that haven't been used by a member in the last #{months_reported} months", "", ""]
+      csv << ["Mens numbers that haven't been used by a member in the last #{months_reported} months - doesn't include new numbers issued last year or uniforms that are currently in stock in the cabinet", "", ""]
       csv << [
-        '"' + (1..55).select{|i| !current_mens_numbers.keys.include?(i) }.join(", ") + '"',
+        '"' + eligible_numbers.select{|i| !current_mens_numbers.keys.include?(i) }.join(", ") + '"',
         '',
         ''
       ]
 
       csv << ["", "", ""]
 
-      csv << ["Womens numbers that haven't been used by a member in the last #{months_reported} months", "", ""]
+      csv << ["Womens numbers that haven't been used by a member in the last #{months_reported} months - doesn't include new numbers issued last year or uniforms that are currently in stock in the cabinet", "", ""]
       csv << [
-        '"' + (1..55).select{|i| !current_womens_numbers.keys.include?(i) }.join(", ") + '"',
+        '"' + eligible_numbers.select{|i| !current_womens_numbers.keys.include?(i) }.join(", ") + '"',
         '',
         ''
       ]
@@ -46,14 +49,16 @@ class MemberDatabase
       csv << ["", "", ""]
 
       csv << ["Mens Number", "# Currently Issued", "Issued to"]
-      current_mens_numbers.each_pair do |number, names|
+      current_mens_numbers.keys.sort.each do |number|
+        names = current_mens_numbers[number]
         csv << [ number, names.uniq.length, names.uniq.join(", ") ]
       end
 
       csv << ["", "", ""]
 
       csv << ["Womens Number", "# Currently Issued", "Issued to"]
-      current_womens_numbers.each_pair do |number, names|
+      current_womens_numbers.keys.sort.each do |number|
+        names = current_womens_numbers[number]
         csv << [ number, names.uniq.length, names.uniq.join(", ") ]
       end
 
