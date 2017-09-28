62d5bdbe3050bba0c338f345d53386e149848efc
Jason Kotchoff <jason@geoplus.com.au>
2017-09-28 22:47:20 +1000

    Configured live API keys for production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,15 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_MUBC_PUBLISHABLE_KEY_TEST'],
-  secret_key: ENV['STRIPE_MUBC_SECRET_KEY_TEST']
-}
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_MUBC_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_MUBC_SECRET_KEY']
+  }
 
-Stripe.api_key = ENV['STRIPE_MUBC_SECRET_KEY_TEST']
+  Stripe.api_key = ENV['STRIPE_MUBC_SECRET_KEY']
+else
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_MUBC_PUBLISHABLE_KEY_TEST'],
+    secret_key: ENV['STRIPE_MUBC_SECRET_KEY_TEST']
+  }
+
+  Stripe.api_key = ENV['STRIPE_MUBC_SECRET_KEY_TEST']
+end
